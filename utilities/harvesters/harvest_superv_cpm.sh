afcf403303727abe8e54231a16407f290577d8d9
Christopher Wingard <cwingard@coas.oregonstate.edu>
2017-10-30 18:01:27 +0000

    Updates, corrections and a new SUNA parser

--- utilities/harvesters/harvest_superv_cpm.sh
+++ utilities/harvesters/harvest_superv_cpm.sh
@@ -7,18 +7,17 @@
 # Wingard, C. 2015-04-17
 
 # Parse the command line inputs
-if [ $# -ne 6 ]; then
+if [ $# -ne 5 ]; then
     echo "$0: required inputs are the platform and deployment names, the name of the CPM,"
-    echo "the subassembly [buoy/nsif/mfn] location of the CPM, the directory base switch"
-    echo "and the name of the file to process."
-    echo "     example: $0 ce01issm D00001 cpm1 buoy 0 20150505.superv.log"
+    echo "the subassembly [buoy/nsif/mfn] location of the CPM, and the name of the file"
+    echo "to process."
+    echo "     example: $0 ce01issm D00001 cpm1 buoy 20150505.superv.log"
     exit 1
 fi
 PLATFORM=${1,,}
 DEPLOY=${2^^}
 CPM=${3,,}
 SUBASY=${4,,}
-BASE=$5
 FILE=`/bin/basename $5`
 
 # Set the default directory paths
@@ -26,8 +25,8 @@ RAW="/home/ooiuser/data/raw"
 PARSED="/home/ooiuser/data/proc"
 PYTHON="/home/ooiuser/bin/conda/bin/python3"
 
-# Setup the input and output filenames as well as the absolute paths
-if [ $BASE == 0 ]; then
+# Setup the input and output file names as well as the absolute paths
+if [ $CPM = "cpm1" ]; then
     IN="$RAW/$PLATFORM/$DEPLOY/cg_data/superv/$FILE"
 else
     IN="$RAW/$PLATFORM/$DEPLOY/cg_data/$CPM/superv/$FILE"
