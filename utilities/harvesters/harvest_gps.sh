771ff33e2234a501792544a4e1ea3d3d34d9b776
Mario Carloni <mcarloni4@gmail.com>
2023-08-21 11:20:30 -0400

    update gps

--- utilities/harvesters/harvest_gps.sh
+++ utilities/harvesters/harvest_gps.sh
@@ -23,7 +23,7 @@ PARSED="/home/ooiuser/data/parsed"
 
 # Setup the input and output filenames as well as the absolute paths
 IN="$RAW/$PLATFORM/$DEPLOY/cg_data/gps/$FILE"
-OUT="$PARSED/$PLATFORM/$DEPLOY/buoy/gps/${FILE%.log}.json"
+OUT="$PARSED/$PLATFORM/$DEPLOY/buoy/gps-1/${FILE%.log}.json"
 if [ ! -d `dirname $OUT` ]; then
     mkdir -p `dirname $OUT`
 fi
