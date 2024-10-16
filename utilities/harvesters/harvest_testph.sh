ae1f276b3fdeedad64cc9345862d52d9e949c951
Christopher Wingard <cwingard@coas.oregonstate.edu>
2024-10-16 12:40:14 -0700

    Finalize harvester script

--- utilities/harvesters/harvest_testph.sh
+++ utilities/harvesters/harvest_testph.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # harvest_testph.sh
 #
-# Reads the raw Deep SeaPHox V2 data files from the Endurance and Pioneer
+# Reads the raw data from a test pH sensors (Sea-Bird Scientific Deep SeaPHox,
+# ANB Sensors OC300, or Idronaut OS310) deployed on Endurance and Pioneer
 # Surface Moorings and create parsed datasets available in JSON formatted
 # files for further processing and review.
 #
 # C. Wingard 2021-10-12 -- Original code
 # C. Wingard 2024-03-08 -- Updated to use the harvest_options.sh script to
 #                          parse the command line inputs
+# C. Wingard 2024-10-16 -- Updated to work with all 3 test pH sensors
 
 # include the help function and parse the required and optional command line options
 DIR="${BASH_SOURCE%/*}"
